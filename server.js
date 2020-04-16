e5b4df58842ae66824b4cb3a7651fb67d6a2cd9d
MilleCodesLN <mille.helene@outlook.fr>
2020-04-16 09:30:25 +0200

    authentification par token Jwt

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 //Imports
 let express = require("express");
+let bodyParser = require("body-parser");
+let apiRouter = require("./apiRouter").router;
 
 //Instantiate server
 let server = express();
+
+//Body Parser configuration
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 //Configure routes
 server.get("/", function (req, res) {
   res.setHeader("Content-Type", "text/html");
   res.status(200).send("<h1>Bonjour sur mon serveur</h1>");
 });
+
+server.use("/api/", apiRouter);
 
 //Launch server
 server.listen(8088, function () {
